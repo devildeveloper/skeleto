50d3607ffb262d301f3a3b62bf3c961f3c420806
devildeveloper <devx.cry@gmail.com>
2014-12-28 11:06:44 -0500

    support to coffeescript

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var gulp=require('gulp');
 var server=require('gulp-server-livereload');
 var jade=require('gulp-jade');
 var stylus=require('gulp-stylus');
+var coffee=require('gulp-coffee');
+var gutil=require('gulp-util');
 
 gulp.task('jade-compile',function(){
 	gulp.src('source/templates/*.jade')
@@ -18,10 +20,17 @@ gulp.task('stylus-compile',function(){
 		.pipe(gulp.dest('dist/css'));
 });
 
+gulp.task('coffee-compile',function(){
+	gulp.src('source/coffee/*.coffee')
+		.pipe(coffee({bare:true}).on('error',gutil.log))
+		.pipe(gulp.dest('dist/js/'))
+});
+
 gulp.task('watcher',function(){
 	console.log('Watcher is running');
 	gulp.watch('source/templates/*.jade',['jade-compile']);
 	gulp.watch('source/styl/*.styl',['stylus-compile']);
+	gulp.watch('source/coffee/*.coffee',['coffee-compile']);
 });
 
 gulp.task('webserver',function(){
