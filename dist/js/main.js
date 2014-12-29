e0fa8d212a6315a2433eacb117ee63d879bb599c
Héctor Arévalo <Laboratorio@iMac-de-Hector.local>
2014-12-29 17:35:32 -0500

    home al 70%

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,5 @@
 $(document).on("ready",function(){
-    $(".slick").slick({
-        autoplay:true,
-        arrows:false,
-        dots:true
-    });
+
     $(".sk-movil.sk-menu .icon-menu").on("click",function(){
         $(".sk-drop").slideToggle();
     });
