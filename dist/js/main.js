600245676c7aafb30fbb718d004de78167bed789
devildeveloper <devx.cry@gmail.com>
2015-01-03 19:26:02 -0500

    skills

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,12 @@
 $(document).on("ready",function(){
-
+    $(".slick").slick({
+        autoplay:true,
+        fade: true,
+        dots: true,
+        speed:500,
+        cssEase: 'linear',
+        arrows: false
+    })
     $(".sk-movil.sk-menu .icon-menu").on("click",function(){
         $(".sk-drop").slideToggle();
     });
