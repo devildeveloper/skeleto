647d4c0f764cda9a45a7dc85bd42c3622d219255
devildeveloper <devx.cry@gmail.com>
2015-01-05 01:11:02 -0500

    gulp css min and concat task

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ var jade=require('gulp-jade');
 var stylus=require('gulp-stylus');
 var coffee=require('gulp-coffee');
 var gutil=require('gulp-util');
+var gulpconcat=require('gulp-concat');
+var gulpcssmin=require('gulp-minify-css');
 
+//compile
 gulp.task('jade-compile',function(){
 	gulp.src('source/templates/*.jade')
 		.pipe(jade({}))
@@ -17,7 +20,7 @@ gulp.task('stylus-compile',function(){
 		.pipe(stylus({
 			compress:true
 		}))
-		.pipe(gulp.dest('dist/css'));
+		.pipe(gulp.dest('source/tmp'));
 });
 
 gulp.task('coffee-compile',function(){
@@ -25,14 +28,26 @@ gulp.task('coffee-compile',function(){
 		.pipe(coffee({bare:true}).on('error',gutil.log))
 		.pipe(gulp.dest('dist/js/'))
 });
-
+//concat
+gulp.task('css-concat',function(){
+	gulp.src(['source/vendor/css/*.css','source/tmp/main.css'])
+		.pipe(gulpconcat('main.min.css'))
+		.pipe(gulp.dest('source/tmp/'))
+});
+//minify
+gulp.task('css-min',function(){
+	gulp.src('source/tmp/main.min.css')
+		.pipe(gulpcssmin({debug:true}))
+		.pipe(gulp.dest('dist/css/'))
+})
+//watcher
 gulp.task('watcher',function(){
 	console.log('Watcher is running');
 	gulp.watch('source/templates/*.jade',['jade-compile']);
-	gulp.watch('source/styl/*.styl',['stylus-compile']);
+	gulp.watch('source/styl/*.styl',['stylus-compile','css-concat','css-min']);
 	gulp.watch('source/coffee/*.coffee',['coffee-compile']);
 });
-
+//webserver
 gulp.task('webserver',function(){
 	gulp.src('dist')
 		.pipe(server({
